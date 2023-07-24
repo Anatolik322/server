701e18f49669c683c72de7776b119948b5401036
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-24 12:47:12 +0300

    feat: created teamplates and routing

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const http = require('http');
+const fs = require('fs')
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200,  {'Content-Type': 'text/html; charset=utf-8'})
+   
+    if(req.url == '/'){
+        fs.createReadStream('./teamplates/main.html').pipe(res)
+    }else if(req.url == '/about'){
+        fs.createReadStream('./teamplates/about.html').pipe(res)
+    }
+})
+
+const HOST = 'localhost'
+const PORT = 3001
+
+server.listen(PORT, HOST, () => {
+    console.log(`http://${HOST}:${PORT}`)
+})
